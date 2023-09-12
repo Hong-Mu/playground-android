23ef540ad97f1bad893a220693cdfbba78a411e8
Hong-Mu <hongmuchae@gmail.com>
2023-09-13 03:02:54 +0900

    Add bound service example

--- serviceexample/src/main/java/com/hongmu/serviceexample/MainActivity.kt
+++ serviceexample/src/main/java/com/hongmu/serviceexample/MainActivity.kt
@@ -1,25 +1,49 @@
 package com.hongmu.serviceexample
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
+import android.widget.Toast
 import com.hongmu.serviceexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val TAG = ".MainActivity"
+    }
+
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var myService: MyService
+    private var isBind = false
+    val connection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, iBinder: IBinder?) {
+            val binder = iBinder as MyService.MyBinder
+            myService = binder.getService()
+            isBind = true
+            Log.d(TAG, "onServiceConnected")
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isBind = false
+            Log.d(TAG, "onServiceDisconnected")
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.btnStart.setOnClickListener {
-            startService()
-        }
-
-        binding.btnStop.setOnClickListener {
-            stopService()
-        }
+        binding.btnStart.setOnClickListener { startService() }
+        binding.btnStop.setOnClickListener { stopService() }
+        binding.btnBind.setOnClickListener { bindService() }
+        binding.btnUnbind.setOnClickListener { unbindService() }
+        binding.btnCall.setOnClickListener { callServiceFunction() }
     }
 
     private fun startService() {
@@ -30,5 +54,26 @@ class MainActivity : AppCompatActivity() {
 
     private fun stopService() {
         stopService(Intent(this, MyService::class.java))
+    }
+
+    private fun bindService() {
+        val intent = Intent(this, MyService::class.java)
+        bindService(intent, connection, Context.BIND_AUTO_CREATE)
+    }
+
+    private fun unbindService() {
+        if (isBind) {
+            unbindService(connection)
+            isBind = false
+        }
+    }
+
+    private fun callServiceFunction() {
+        if(isBind) {
+            val message = myService.getMessage()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "서비스가 연결되지 않음", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- serviceexample/src/main/java/com/hongmu/serviceexample/MyService.kt
+++ serviceexample/src/main/java/com/hongmu/serviceexample/MyService.kt
@@ -2,9 +2,9 @@ package com.hongmu.serviceexample
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
-import android.widget.Toast
 
 class MyService : Service() {
 
@@ -15,12 +15,23 @@ class MyService : Service() {
         const val ACTION_STOP = "com.hongmu.serviceexample.STOP"
     }
 
-    override fun onBind(intent: Intent): IBinder? {
-        return null
+    inner class MyBinder : Binder() {
+        fun getService() = this@MyService
+    }
+
+    private val binder = MyBinder()
+
+    fun getMessage(): String {
+        return "Hello from Service"
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        Log.d(TAG, "onBind")
+        return binder
     }
 
     override fun onCreate() {
-        Log.d(TAG, "onCreate: 서비스 생성")
+        Log.d(TAG, "onCreate")
         super.onCreate()
     }
 
@@ -31,7 +42,17 @@ class MyService : Service() {
     }
 
     override fun onDestroy() {
-        Log.d(TAG, "onDestroy: 서비스 종료")
+        Log.d(TAG, "onDestroy")
         super.onDestroy()
+    }
+
+    override fun onRebind(intent: Intent?) {
+        Log.d(TAG, "onRebind")
+        super.onRebind(intent)
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        Log.d(TAG, "onUnbind")
+        return super.onUnbind(intent)
     }
 }
